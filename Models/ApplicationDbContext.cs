0b43e57011f29c241631844b5397da188ca46a8c
scottapow <lfh.scot@gmail.com>
2016-02-20 12:16:18 -0500

    Started the migration from VS to Code using Yeoman aspnet generator

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Data.Entity;
+
+namespace WaitlistManager.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Visit> Visits { get; set; }
+        public DbSet<Barber> Barbers { get; set; }
+        
+        public ApplicationDbContext()
+        {
+            Database.EnsureCreated();
+        }
+        
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
